ba1ef7cdf1b62d394f5daf55b89626caaeb82b16
sireen-khan <113620901+sireen-khan@users.noreply.github.com>
2023-02-09 12:30:31 -0500

    Add files via upload

--- pygame/click_game.py
+++ pygame/click_game.py
@@ -1,5 +1,5 @@
-my_character = Actor('character')
-my_character.pos = 100,50
+my_character = Actor('square')
+my_character.topright = 0, 10
 
 WIDTH = 500
 HEIGHT = my_character.height + 20
@@ -10,8 +10,18 @@ def draw():
     my_character.draw()
 
 def update():
-    my_character.left = my_character.left += 2
+    my_character.left = my_character.left + 2
     if my_character.left > WIDTH:
-        my_charcter.right = 0
+        my_character.right = 0
+
+def on_mouse_down(pos):
+    if my_character.collidepoint(pos):
+        print("Eek!")
+    else:
+        print("You missed me!")
+def on_mouse_down(pos):
+    if my_character.collidepoint(pos):
+        sounds.eep.play()
+        my_character.image = 'my_character_hurt'
 
 
